ceeb14ba45db0563658d6487cfe46a6f0b0368a1
Enneta <b.guyot7@gmail.com>
2018-05-02 16:01:41 +0200

    detection de la connexion dans le header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,11 @@
 
     <header>
     <nav class="navbar navbar-expand-md navbar-dark essbg1">
-
-        <a class="navbar-brand" href="{{ url('/welcome') }}"><img src="./images/LaPlace-Transparent.png" width="200" height="100" class="d-inline-block align-top" alt=""></a>
+        <a class="navbar-brand" href="#"><img src="./images/LaPlace-Transparent.png" width="200" height="100" class="d-inline-block align-top" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        
           <ul class="navbar-nav">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
@@ -31,23 +29,35 @@
                   <a class="dropdown-item essbg3" href="#">Informatique</a>
                 </div>
               </li>
-              
-              
+            @guest
             <li class="nav-item right-item-nav">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('s inscrire') }}</a>
+            </li>
+            <li class="nav-item right-item-nav-end">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('se connecter') }}</a>
+            </li>
+            @else
+            <li class="nav-item dropdown right-item-nav">
+            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                    {{ Auth::user()->name }} </span>
+                                </a>
 
-                <a class="nav-link" href="{{ route('register') }}">S'inscrire</a>
-            </li>
+                                <div class="dropdown-menu essbg2">
+                                    <a class="dropdown-item essbg3" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+            @endguest
             <li class="nav-item right-item-nav-end">
-                <a class="nav-link" href="{{ route('login') }}">Se connecter</a>
+                <a class="nav-link" href="#">Nous contacter</a>
             </li>
-            <li class="nav-item right-item-nav-end">
-                <a class="nav-link" href="{{ route('contact') }}">Nous contacter</a>
-           
-            </li>
-            <!-- <li class="nav-item right-item-nav-end">
-                <a class="nav-link" href="{{ route('logout') }}">Se déconnecter</a>
-            </li> -->
-         
           </ul>
         </div>  
     </nav>
