7bb7b53af54f4fa4f0e92510717f311c9d7f8e19
Enneta <b.guyot7@gmail.com>
2018-05-24 17:58:57 +0200

    ajout de data.sql + generation Search form

--- resources/views/layouts/userLayout.blade.php
+++ resources/views/layouts/userLayout.blade.php
@@ -1,6 +1,14 @@
 <?php 
 use App\User;
 $type = Auth::user()->type;
+//generation de la selection de competence
+$comps = Skills::all();
+$searchForm ="echo Form::select('id', array(";
+foreach( $comps as $comp ){
+  $searchForm = $searchForm."'".$comp->id."' => '".$comp->nom."',";
+}
+$searchForm = $searchForm."));";
+
 ?>
 
 @include('layouts.head')
